6f7ceefff4de8812996028c5732b280212763198
RichardKavanagh <richard.kavanagh7@mail.dcu.ie>
2015-02-04 18:21:43 +0000

    Removing unnecesssary files.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m += hello_world.o
+obj-m += hidden.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- make.c
+++ make.c
@@ -1,4 +1,4 @@
-Obj-m := hello_world.o
+Obj-m := hidden.o
 KERNEL_DIR = /lib/modules/$(shell uname -r)/build
 PWD = $(shell PWD)
 all:
